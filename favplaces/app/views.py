a07ef79c74d71c5e45f317bd27d92ba4797e1b9d
Wiktor Koźlik <wiktor.kozlik1@gmail.com>
2025-06-01 22:59:13 +0200

    added pagination for history.html page

--- favplaces/app/views.py
+++ favplaces/app/views.py
@@ -9,6 +9,8 @@ from datetime import datetime
 from django.core.paginator import Paginator
 import os
 from django.conf import settings
+from itertools import islice
+from collections import defaultdict
 
 def rejestracja(request):
     if request.method == 'POST':
@@ -101,6 +103,7 @@ def strona_glowna(request):
     return render(request, 'app/strona_glowna.html', {'uzytkownik': uzytkownik})
 
 def historia(request):
+    biezaca_data = None
     uzytkownik = None
     historia_lista = []
     uzytkownik_id = request.session.get('uzytkownik_id')
@@ -110,10 +113,11 @@ def historia(request):
             query = request.GET.get('q', '')
             data_wyszukiwania = request.GET.get('data_wyszukiwania', '')
             historia_qs = HistoriaWyszukiwan.objects.filter(ID_Użytkownik=uzytkownik).order_by('-Data_wyszukiwania')
+
             if query:
                 historia_qs = historia_qs.filter(
                     Q(ID_Miejsca__Nazwa__icontains=query) |
-                    Q(ID_Miejsca__Miejscowość__icontains(query))
+                    Q(ID_Miejsca__Miejscowość__icontains=query)
                 )
             if data_wyszukiwania:
                 try:
@@ -121,14 +125,37 @@ def historia(request):
                     historia_qs = historia_qs.filter(Data_wyszukiwania__date=data_obj)
                 except ValueError:
                     pass
-            historia_lista = historia_qs
+
+            # Grupowanie po dacie (bez godziny)
+            historia_dict = defaultdict(list)
+            for wpis in historia_qs:
+                data_key = wpis.Data_wyszukiwania.date()
+                historia_dict[data_key].append(wpis)
+
+            # Posortuj daty malejąco
+            historia_by_day = sorted(historia_dict.items(), key=lambda x: x[0], reverse=True)
+
+            # Paginacja po dniach
+            paginator = Paginator(historia_by_day, 1)  # 1 dzień = 1 strona
+            page_number = request.GET.get('page')
+            page_obj = paginator.get_page(page_number)
+
+            # Bieżący dzień
+            biezaca_data = page_obj.object_list[0][0]
+
+            # Bieżąca lista wpisów z tego dnia
+            historia_lista = page_obj.object_list[0][1] if page_obj.object_list else []
+
         except Uzytkownik.DoesNotExist:
             request.session.flush()
+
     return render(request, 'app/historia.html', {
         'uzytkownik': uzytkownik,
         'historia_lista': historia_lista,
         'query': request.GET.get('q', ''),
         'data_wyszukiwania': request.GET.get('data_wyszukiwania', ''),
+        'page_obj': page_obj if uzytkownik else None,
+        'biezaca_data': biezaca_data,
     })
 
 def ranking(request):
